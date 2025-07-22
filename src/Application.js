a5a50202b0e8ade80ba426ae7190c14dbbfb8ab9
kaurcev <kaurcev@kaurcev.kaurcev>
2025-07-22 11:17:17 +0600

    blya

--- src/Application.js
+++ src/Application.js
@@ -3,23 +3,24 @@ import { useNavigate } from "react-router-dom";
 import pkg from '../package.json';
 import en from './localization/en.json';
 import ru from './localization/ru.json';
+import logo from './imgs/logo.png';
 
 const AppContext = createContext();
 
 export const AppProvider = ({ children }) => {
   const navigate = useNavigate();
   const [title, setTitle] = useState("");
-  
+
   // Состояние для языка и переводов
   const [language, setLanguage] = useState('ru');
   const [labels, setLabels] = useState(ru);
-  
+
   const api_url = pkg.public_api;
   const version = pkg.version;
   const versionstate = pkg.versionstate;
   const author = pkg.author;
   const kaurcevdev = pkg.kaurcevdev;
-  const github = pkg.github; 
+  const github = pkg.github;
 
   // Загрузка переводов при изменении языка
   useEffect(() => {
@@ -33,17 +34,18 @@ export const AppProvider = ({ children }) => {
 
   return (
     <AppContext.Provider value={{
-      navigate, 
-      setTitle, 
-      version, 
-      versionstate, 
-      author, 
-      kaurcevdev, 
-      github, 
+      navigate,
+      setTitle,
+      version,
+      versionstate,
+      author,
+      kaurcevdev,
+      github,
       api_url,
       language,
       setLanguage,
-      labels
+      labels,
+      logo
     }}>
       {children}
     </AppContext.Provider>
--- src/components/header/comp.js
+++ src/components/header/comp.js
@@ -2,17 +2,17 @@ import { useAppContext } from '../../Application';
 import './style.css';
 
 const Header = () => {
-    const {navigate, labels} = useAppContext();
+    const { navigate, labels, logo } = useAppContext();
     return (
         <>
             <header>
                 <div className="header">
-                    <div onClick={()=>navigate("/")} className='title'>
-                        {labels["project_name"]}
+                    <div onClick={() => navigate("/")} className='title'>
+                        <img className="logo" src={logo} alt={labels["project_name"]} />
                     </div>
                 </div>
             </header>
-        </> 
+        </>
     );
 }
 
